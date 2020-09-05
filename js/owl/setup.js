e02361017c40a69aaad654feea31156798b011a8
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-09-05 14:22:09 -0300

    Adiciona autoplay no carrossel

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -4,6 +4,9 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
